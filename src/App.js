ac8b939ec41a93828f62fddc9342e89394aac20e
Travis Riegler <travisriegler@protonmail.com>
2023-09-18 13:04:22 -0400

    fixed axios issue with jest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 import baseUrl from './config'
+import React, { useEffect } from 'react';
 
 const App = () => {
 
-  axios(`${baseUrl}/api/v1/books`).then(books => {
-    console.log(books);
-  })
+  useEffect(() => {
+    axios(`${baseUrl}/api/v1/books`)
+      .then((response) => {
+        //console.log(response.data);
+      })
+      .catch((error) => {
+        //console.error(error);
+      });
+  }, []);
 
   return <div>My first component</div>
 }
